6bbe39b1707d9c0d8cedc5e6d0451cd0057806a6
suryavummadi <surya.vummadi@cogni-ext.com>
2025-02-13 18:03:06 +0530

    first commit

---
+++ Sources/SwiftUtility/SUDateFormatter.swift
@@ -0,0 +1,27 @@
+//
+//  File.swift
+//  
+//
+//  Created by Surya Vummadi on 13/02/25.
+//
+
+import Foundation
+
+open class SUDateFormatter {
+    static let shared = SUDateFormatter()
+
+    public enum DateFormatterStyle: String {
+        case OnlyDate = "yyyy-MM-dd"
+        case OnlyTime = "HH:mm:ss"
+    }
+
+    public init() { }
+
+   public static func getDateString(date: Date, format: DateFormatterStyle) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = format.rawValue
+
+        let dateString = dateFormatter.string(from: date)
+        return dateString
+    }
+}
